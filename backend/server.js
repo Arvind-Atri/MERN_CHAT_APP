43dc365a2bf85ba11f52f758984683b770278dae
Arvind-Atri <148221394+Arvind-Atri@users.noreply.github.com>
2024-05-03 16:25:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ const server = app.listen(PORT,console.log("runnninnnnnng"))
 const io=require("socket.io")(server,{
     pingTimeout:60000,
     cors:{
-        origin:`http://localhost:3000`,
+        origin:`*`,
     },
 })
 
